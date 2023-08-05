503da45ca5b49213b21ef8c72058048da8613d8e
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-08-06 02:31:48 +0530

    add: filter animation on image gallery

---
+++ filter-animation/src/utils.js
@@ -0,0 +1,29 @@
+export const genres = [
+  {
+    id: 0,
+    name: "All",
+  },
+  {
+    id: 28,
+    name: "Action",
+  },
+
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+
+  {
+    id: 10749,
+    name: "Romance",
+  },
+
+  {
+    id: 53,
+    name: "Thriller",
+  },
+];
